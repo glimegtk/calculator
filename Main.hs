a17b620a1b0ac59c3e0e7183a4940e74276dc5ea
Niladri Das <bniladridas@users.noreply.github.com>
2026-02-16 05:25:35 +0530

    feat: haskell gtk calculator app

---
+++ Main.hs
@@ -0,0 +1,103 @@
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.GI.Base
+import qualified GI.Gtk as Gtk
+import Control.Monad (void)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.IORef
+import Text.Read (readMaybe)
+
+data CalcState = CalcState
+  { calcDisplay :: Text
+  , firstOp :: Maybe Double
+  , pendingOp :: Maybe (Double -> Double -> Double)
+  }
+
+emptyState :: CalcState
+emptyState = CalcState "" Nothing Nothing
+
+main :: IO ()
+main = do
+  Gtk.init Nothing
+
+  win <- new Gtk.Window [#title := "Calculator", #resizable := False]
+  on win #destroy Gtk.mainQuit
+
+  box <- new Gtk.Box [#orientation := Gtk.OrientationVertical]
+  #add win box
+
+  entry <- Gtk.entryNew
+  #packStart box entry True True 5
+
+  grid <- Gtk.gridNew
+  Gtk.gridSetRowSpacing grid 5
+  Gtk.gridSetColumnSpacing grid 5
+  #packStart box grid True True 5
+
+  stVar <- newIORef emptyState
+
+  let addButton lbl row col = do
+        btn <- new Gtk.Button [#label := lbl]
+        Gtk.gridAttach grid btn col row 1 1
+        on btn #clicked $ handleInput entry stVar lbl
+
+  let addOpButton lbl row col op = do
+        btn <- new Gtk.Button [#label := lbl]
+        Gtk.gridAttach grid btn col row 1 1
+        on btn #clicked $ handleOp entry stVar op
+
+  addButton "7" 0 0
+  addButton "8" 0 1
+  addButton "9" 0 2
+  addOpButton "/" 0 3 (\a b -> a / b)
+
+  addButton "4" 1 0
+  addButton "5" 1 1
+  addButton "6" 1 2
+  addOpButton "*" 1 3 (\a b -> a * b)
+
+  addButton "1" 2 0
+  addButton "2" 2 1
+  addButton "3" 2 2
+  addOpButton "-" 2 3 (\a b -> a - b)
+
+  addButton "0" 3 0
+  addButton "." 3 1
+  addOpButton "=" 3 2 (\a _ -> a)
+  addOpButton "+" 3 3 (\a b -> a + b)
+
+  clearBtn <- new Gtk.Button [#label := "C"]
+  Gtk.gridAttach grid clearBtn 0 4 4 1
+  on clearBtn #clicked $ do
+    writeIORef stVar emptyState
+    Gtk.entrySetText entry ""
+
+  #showAll win
+  Gtk.main
+
+handleInput :: Gtk.Entry -> IORef CalcState -> Text -> IO ()
+handleInput display stRef input = do
+  st <- readIORef stRef
+  let currentText = calcDisplay st
+  let newDisplay = case input of
+        "." -> if T.isInfixOf "." currentText then currentText else T.append currentText input
+        _   -> T.append currentText input
+  writeIORef stRef st { calcDisplay = newDisplay }
+  Gtk.entrySetText display newDisplay
+
+handleOp :: Gtk.Entry -> IORef CalcState -> (Double -> Double -> Double) -> IO ()
+handleOp display stRef op = do
+  st <- readIORef stRef
+  let currentText = calcDisplay st
+  let current = readMaybe (T.unpack currentText) :: Maybe Double
+  case (firstOp st, current) of
+    (Nothing, Just n) -> do
+      writeIORef stRef st { firstOp = Just n, pendingOp = Just op, calcDisplay = "" }
+      Gtk.entrySetText display ""
+    (Just n, Just m) -> do
+      let result = op n m
+      Gtk.entrySetText display (T.pack (show result))
+      writeIORef stRef st { firstOp = Just result, pendingOp = Just op, calcDisplay = "" }
+    _ -> return ()
